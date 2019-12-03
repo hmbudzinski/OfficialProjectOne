5b676ce064f841e29e7925cd8dc7596b6ca99f04
Cindy Burton <cindy.brton@gmail.com>
2019-12-02 20:55:07 -0700

    started the front end pseudo code

---
+++ assets/frontScript.js
@@ -0,0 +1,7 @@
+// first we need to collect the user's search term as a dynamic variable
+
+// then we need to input that search variable 
+
+// then we need to get the info from the ajax response as variables
+
+// then manipulate the DOM to display the recipe and the corresponding gif
