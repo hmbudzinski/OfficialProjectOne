c69be33943f13934994ea953aab479ae86561f35
Hayley Budzinski <hayleybudzinski@gmail.com>
2019-12-05 14:03:23 -0700

    added code to append the search terms to the 'local storage' div on the page

--- assets/javascript/search.js
+++ assets/javascript/search.js
@@ -7,6 +7,7 @@ var additionalKeys =
 	"%20dessert&to=3&nutrients[SUGAR]=20%2B&enum[crustacean-free]&enum[fish-free]&enum[pork-free]&enum[red-meat-free]&enum[shellfish-free]";
 var idAndKey = "&app_id=2790841b&app_key=f439766755de77a40d2e25c9776f23a1";
 var userSearched;
+var list = [];
 
 // grab the first three objects with the info we need
 // recipe title, recipe image, and recipe url
@@ -50,7 +51,7 @@ function searchRecipe(searchTerm) {
 			console.log("Share URL " + i + " " + shareUrl);
 
 			// //append to recipe div
-			// $("#recipe-info").append(title + img + shareUrl + "<br>");
+			$("#recipe-info").append(title + img + shareUrl + "<br>");
 		}
 
 		//create objects for each of the three hits?
@@ -68,36 +69,35 @@ function searchRecipe(searchTerm) {
 /////
 
 //modified on click to show as event 12/5 8:27am - jdr
-$("#searchButton").on("click", function(event) {
+$("#search-button").on("click", function(event) {
 	event.preventDefault();
-
+	$("#recipe-info").empty();
 	// moved searchTerm from global to inside of event 12/5 jdr
 	var searchTerm = $("#search-term").val();
-	console.log(searchTerm);
-
-	//creating a button and setting the user input value to button
-	userSearched = $("<button>").text($("#search-term").val());
-	console.log(userSearched);
-
-	//adding class to new buttons
-	userSearched.addClass("ul");
-	userSearched.attr("id", $("#search-term").val());
-
-	//creating an unordered list to hold the new buttons
-	var list = $("<ul>");
-
 	//appending buttons to new unordered list
-	list.append(userSearched);
+	list.push(searchTerm);
+	
+	for(var i = 0; i <list.length; i++){
+		$("#localStorage").append($("<br>"));
+		//creating a button and setting the user input value to button
+		//replaces all dashes with spaces
+		userSearched = $("<button>").text(list[i]);
+		console.log(userSearched);
+		
+		//adding class to new buttons
+		userSearched.addClass("ul");
+		//string replace function, to replace all spaces with dashes
+		userSearched.attr("id", list[i]);
+		$("#localStorage").append(userSearched);
+	}
 
-	//appending unordered list to body of app
-	$("body").append(list);
-
-	// localStorage.setItem("label" + searchTerm, searchTerm);
-
+	//need to use what Jody grabs from the recipe to store to local storage
+	//put three recipies in an array when they are grabbed, and then store that array with the search term
+	// localStorage.setItem(searchTerm, //returned array of recipies);
+	//need to loop through what API has returned and create array of returns to store in local storage
 	//clearing the search bar
 	$("#search-term").val("");
 
-	//calling search recipe function
 	//passed search term to function 12/5 826am - jdr
 	searchRecipe(searchTerm);
 });
