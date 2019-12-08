998ec2bbb3f24aa6ba8a5767a4b627924f357e16
Cindy Burton <cindy.brton@gmail.com>
2019-12-07 19:22:43 -0700

    working on the search restrictions

--- assets/javascript/search.js
+++ assets/javascript/search.js
@@ -6,27 +6,69 @@ var urlStart = "https://api.edamam.com/search?q=";
 var additionalKeys =
 	"%20dessert&to=3&nutrients[SUGAR]=20%2B&enum[crustacean-free]&enum[fish-free]&enum[pork-free]&enum[red-meat-free]&enum[shellfish-free]";
 var idAndKey = "&app_id=2790841b&app_key=f439766755de77a40d2e25c9776f23a1";
+var vegan = "&health=vegan"
+var peanutFree = "&health=peanut-free"
+var treeNutFree = "&health=tree-nut-free"
+// var fatFree ="&health=fat-free"
 var userSearched;
 var list = [];
+var ischecked = false;
+var value;
+
+
+// checking the checkbox's checked or unchecked status
+$("input:checkbox").change(function () {
+	ischecked = $(this).is(":checked")
+	value = $(this).val();
+	console.log("checked", ischecked, value)
+})
 
 /////
 // reusable functions
 /////
 
+
+
 function searchRecipe(searchTerm) {
 	//URI encode to change spaces into %20
+
 	searchThis = encodeURIComponent(searchTerm);
 	console.log("Search this: " + searchThis);
 	$("#recipe-info").empty();
 	//create search url and console out
 	var finalUrl = urlStart + searchThis + additionalKeys + idAndKey;
+
+	// if statements to detect which boxes are checked to alter the queryURL
+	if (ischecked === true && value === "vegan") {
+		console.log("checked is true", value)
+
+		finalUrl = urlStart + searchThis + additionalKeys + vegan + idAndKey
+	}
+
+	if (ischecked === true && value === "peanut-free") {
+		console.log("checked is true", value)
+
+		finalUrl = urlStart + searchThis + additionalKeys + peanutFree + idAndKey
+	}
+
+	if (ischecked === true && value === "tree-nut-free") {
+		console.log("checked is true", value)
+
+		finalUrl = urlStart + searchThis + additionalKeys + treeNutFree + idAndKey
+	}
+
+	// if (ischecked === true && value === "vegan" && value === "peanut-free") {
+	// 	finalUrl = urlStart + searchThis + additionalKeys + vegan + + idAndKey
+	// }
+
+
 	console.log(finalUrl);
 
 	//ajax call the url
 	$.ajax({
 		url: finalUrl,
 		method: "GET"
-	}).then(function(response) {
+	}).then(function (response) {
 		for (var i = 0; i < response.hits.length; i++) {
 			// go through the json object and grab three of the recipes, images and titles
 			// title
@@ -75,7 +117,7 @@ function sideButtons() {
 	}
 
 	// $("#search-term").val("");
-	$(".prevSearch").on("click", function(event) {
+	$(".prevSearch").on("click", function (event) {
 		event.preventDefault();
 		console.log("click");
 		var searchTerm = $(this).data("id");
@@ -92,7 +134,7 @@ function sideButtons() {
 //events
 /////
 
-$("#search-button").on("click", function(event) {
+$("#search-button").on("click", function (event) {
 	event.preventDefault();
 
 	// moved searchTerm from global to inside of event 12/5 jdr
