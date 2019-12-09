d3f6d8e00d9bb14e0fb1ad39f3368033625f9d5b
Bri Gab <brigabiola@outlook.com>
2019-12-09 13:47:21 -0700

    commiting to get up to date

--- assets/javascript/frontScript.js
+++ assets/javascript/frontScript.js
@@ -6,93 +6,81 @@
 
 // then manipulate the DOM to display the recipe and the corresponding gif
 
-console.log(searchbox)
+var checkboxVal = [];
 
 $("#search-button").on("click touch", function (event) {
-    $("#recipe-info").remove();
-    $("#stored").remove();
-    $("#joke").remove();
+    event.preventDefault();
+    $("#recipe-info").empty();
+    $("#stored").empty();
+    $("#joke").empty();
 
-    var searchbox = $("#searchbox");
+    // used for finding the value of multiple checkbox ticks for the search
+    $("input:checkbox:checked").each(function () {
+        val = $(this).val();
+        console.log("checkbox value? ", $(this).val())
+        checkboxVal.push(val)
+        console.log("checkboxVal array ", checkboxVal)
+    })
 
 
-    searchbox.animate({ height: "100px", margin: "0 0 20px 0", position: "absolute", top: "20px", padding: "0px" })
-    $("#shrink").animate({ fontSize: "22pt" });
-    $("#shrink").addClass("afterShrink")
+
+
+    $("#searchbox").animate({ height: "150px", margin: "0 0 20px 0", marginTop: "20px", padding: "10px 0 50px 0" });
+    $("#shrink-text").animate({ fontSize: "22pt" });
     $(".row").removeClass("extra-padding");
     $("#search-term").animate({ height: "20px" });
     $("#search-button").animate({ fontSize: "12pt" });
-    $("#search-button").removeClass("searchCenter")
-    var newContainer = $("<div class='container'>");
+    $("#search-button").removeClass("searchCenter");
 
-    newContainer.attr("id", "results");
-    newContainer.attr("class", "container center");
-    newContainer.insertAfter(searchbox);
-    var newRow = $("<div class='row'>");
-    newContainer.append(newRow);
+    $("#recipe-box").fadeIn(1000, "swing");
+    $("#recipe-info").fadeIn(1000, "swing");
+    $("#stored").fadeIn(1000, "swing");
+    $("#joke").fadeIn(1000, "swing");
+    // var newContainer = $("<div class='container'>");
 
-    divIds = ["recipe-info", "stored", "joke"]
-    for (var i = 0; i < divIds.length; i++) {
-        // var textArr = ["<span><h4>Recipe Title</h4></span><span>[img goes here]</span><span><h4>Recipe Title</h4></span><span>[img goes here]</span><span><h4>Recipe Title</h4></span><span>[img goes here]</span>", "<span><h4>Local Storage</h4></span><span></span>", "<span><h5>Don't Forget to Laugh!</h5></span>"]
-        var textArr =[];
-        var newCol = $("<div class='six columns trans-bg'>");
-        newCol.attr("id", divIds[i])
-        newRow.append(newCol);
-        newCol.html(textArr[i]);
-    }
+    // newContainer.attr("id", "results");
+    // newContainer.attr("class", "container center");
+    // newContainer.insertAfter(searchbox);
+    // var newRow = $("<div class='row'>");
+    // newContainer.append(newRow);
+
+    // divIds = ["recipe-info", "stored", "joke"]
+    // for (var i = 0; i < divIds.length; i++) {
+    //     // var textArr = ["<span><h4>Recipe Title</h4></span><span>[img goes here]</span><span><h4>Recipe Title</h4></span><span>[img goes here]</span><span><h4>Recipe Title</h4></span><span>[img goes here]</span>", "<span><h4>Local Storage</h4></span><span></span>", "<span><h5>Don't Forget to Laugh!</h5></span>"]
+    //     // var textArr = [];
+    //     var newCol = $("<div class='trans-bg'>");
+    //     newCol.attr("id", divIds[i])
+    //     newRow.append(newCol);
+    //     // newCol.html(textArr[i]);
+    // }
+
+    // $("#recipe-info").addClass("eight columns");
+    // $("#stored").addClass("four columns")
+    // $("#joke").addClass("four columns")
 
     getJoke();
 })
 
-// This allows the user to press enter instead of clicking search
-$("#searchbox").keyup(function(event){
-    if(event.keyCode == 13){
+$("#searchbox").keyup(function (event) {
+    if (event.keyCode == 13) {
         $("#search-button").click();
     }
 });
 
-function getJoke (){
-// $("#joke").removeClass("six columns")
-// $("#joke").addClass("four columns offset-by-four top-margin20")
+function getJoke() {
+    // $("#joke").removeClass("six columns")
+    // $("#joke").addClass("four columns offset-by-four top-margin20")
 
 
-var url = "https://api.spoonacular.com/food/jokes/random?&apiKey=349d5f926732476ab8ac52e9787cedc9"
-$.ajax({
-    url: url,
-    method: "GET"
-}).then(function (response) {
-    console.log(response.text)
-    var jokeBox = $("<div>")
-    jokeBox.addClass("jokes")
-    jokeBox.text(response.text)
-    $("#joke").append(jokeBox)
-    
-    getGiphys();
-})
-}
-
-var searchTerm = $("#search-term").val(); 
-
-
-function getGiphys(){
-
-    var apiKey = "2OFRYxUYBmo81pSfHQf7sRWZrHLq6NPe"
-    
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&api_key=" + apiKey + "&limit=1&rating=G"
-
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function(response) {
-    console.log(searchTerm)
-    console.log(response)
-    var giph = response.data[0].images.fixed_height.url
-    var giphBox = $("<div>")
-    // console.log("div creation ",giphBox);
-    var image = $("<img>").attr("src", giph) 
-    console.log("image and source ",image);
-    giphBox.append(image)
-    // console.log("giph box line " + giphBox)
-    $("#joke").append(giphbox)
-});
+    var url = "https://api.spoonacular.com/food/jokes/random?&apiKey=349d5f926732476ab8ac52e9787cedc9"
+    $.ajax({
+        url: url,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response.text)
+        var jokeBox = $("<div>")
+        jokeBox.addClass("jokes")
+        jokeBox.text(response.text)
+        $("#joke").append(jokeBox)
+    })
 }
--- assets/javascript/search.js
+++ assets/javascript/search.js
@@ -6,8 +6,24 @@ var urlStart = "https://api.edamam.com/search?q=";
 var additionalKeys =
 	"%20dessert&to=3&nutrients[SUGAR]=20%2B&enum[crustacean-free]&enum[fish-free]&enum[pork-free]&enum[red-meat-free]&enum[shellfish-free]";
 var idAndKey = "&app_id=2790841b&app_key=f439766755de77a40d2e25c9776f23a1";
+var vegan = "&health=vegan"
+var peanutFree = "&health=peanut-free"
+var treeNutFree = "&health=tree-nut-free"
+// var fatFree ="&health=fat-free"
 var userSearched;
 var list = [];
+var ischecked = false;
+var value;
+
+
+// checking the checkbox's checked or unchecked status
+$("input:checkbox").change(function () {
+	ischecked = $(this).is(":checked")
+	value = $(this).val();
+	console.log("checked", ischecked, value)
+});
+
+
 
 /////
 // reusable functions
@@ -15,18 +31,69 @@ var list = [];
 
 function searchRecipe(searchTerm) {
 	//URI encode to change spaces into %20
+
 	searchThis = encodeURIComponent(searchTerm);
 	console.log("Search this: " + searchThis);
 	$("#recipe-info").empty();
 	//create search url and console out
 	var finalUrl = urlStart + searchThis + additionalKeys + idAndKey;
+
+	// if statements to detect which boxes are checked to alter the queryURL
+	if (ischecked === true && value === "vegan") {
+		console.log("checked is true", value)
+
+		finalUrl = urlStart + searchThis + additionalKeys + vegan + idAndKey
+
+		checkboxVal = [];
+	}
+
+	if (ischecked === true && value === "peanut-free") {
+		console.log("checked is true", value)
+
+		finalUrl = urlStart + searchThis + additionalKeys + peanutFree + idAndKey
+		checkboxVal = [];
+	}
+
+	if (ischecked === true && value === "tree-nut-free") {
+		console.log("checked is true", value)
+
+		finalUrl = urlStart + searchThis + additionalKeys + treeNutFree + idAndKey
+		checkboxVal = [];
+	}
+
+	if (ischecked === true && checkboxVal === "vegan", "peanut-free") {
+		console.log("user checked vegan and peanut-free")
+		finalUrl = urlStart + searchThis + additionalKeys + vegan + peanutFree + idAndKey
+		checkboxVal = [];
+	}
+
+	if (ischecked === true && checkboxVal === "vegan", "tree-nut-free") {
+		console.log("user checked vegan and tree-nut-free")
+		finalUrl = urlStart + searchThis + additionalKeys + vegan + treeNutFree + idAndKey
+		checkboxVal = [];
+	}
+
+	if (ischecked === true && checkboxVal === "peanut-free", "tree-nut-free") {
+		console.log("user checked peanut-free and tree-nut-free")
+		finalUrl = urlStart + searchThis + additionalKeys + peanutFree + treeNutFree + idAndKey
+		checkboxVal = [];
+	}
+
+	if (ischecked === true && checkboxVal === "vegan", "peanut-free", "tree-nut-free") {
+		console.log("user checked vegan, peanut-free, and tree-nut-free")
+		finalUrl = urlStart + searchThis + additionalKeys + vegan + peanutFree + treeNutFree + idAndKey
+		checkboxVal = [];
+	}
+
+
+
 	console.log(finalUrl);
 
 	//ajax call the url
 	$.ajax({
 		url: finalUrl,
 		method: "GET"
-	}).then(function(response) {
+	}).then(function (response) {
 		for (var i = 0; i < response.hits.length; i++) {
 			// go through the json object and grab three of the recipes, images and titles
 			// title
@@ -52,7 +119,40 @@ function searchRecipe(searchTerm) {
 			buttonLink.text("Click here for recipe");
 			$("#recipe-info").append(urlButton);
 		}
+
+		if (response.hits.length === 0) {
+
+
+		$("#search-term").attr("placeholder", "Search for a Real Word Dummy!")
+		$("#recipe-info").hide();
+		$("#recipe-box").hide();
+		$("#stored").hide();
+		$("#joke").hide();
+
+
+		} else {
+			$("#search-term").attr("placeholder", "Search for a Dessert!");
+
+			var containsTerm = false;
+
+			if (list != null) {
+				$(list).each(function (x) {
+					if (list[x] === searchTerm) {
+						containsTerm = true;
+					}
+				});
+			}
+
+			//push search term to the list array
+
+			if (containsTerm === false) {
+				list.push(searchTerm);
+			}
+		}
+		sideButtons();
+
 	});
+
 }
 
 function sideButtons() {
@@ -61,12 +161,13 @@ function sideButtons() {
 
 	for (var i = 0; i < list.length; i++) {
 		$("#stored").append($("<br>"));
+
 		//creating a button and setting the user input value to button
 		//replaces all dashes with spaces
 		userSearched = $("<button>");
-		console.log("is it a button: " + userSearched);
+		// console.log("is it a button: " + userSearched);
 		userSearched.text(list[i]);
-		console.log(userSearched);
+		// console.log(userSearched);
 		//adding class to new buttons
 		userSearched.addClass("prevSearch");
 		//string replace function, to replace all spaces with dashes
@@ -74,67 +175,169 @@ function sideButtons() {
 		$("#stored").append(userSearched);
 	}
 
-	// $("#search-term").val("");
-	$(".prevSearch").on("click", function(event) {
+	$(".prevSearch").on("click", function (event) {
 		event.preventDefault();
-		console.log("click");
+		// console.log("click");
 		var searchTerm = $(this).data("id");
 		console.log("localStorage" + searchTerm);
 		searchRecipe(searchTerm);
 	});
+
+	var searchedTitle = $("<h5>");
+	searchedTitle.text("Your Delicious Searches");
+	$("#stored").prepend(searchedTitle);
+	
+	var clearButton = $("<button>");
+	clearButton.attr("id", "clearButton");
+	clearButton.append($('<i class="fas fa-times"></i>'));
+	$("#stored").append(clearButton);
+
+	$("#clearButton").on("click", function (event) {
+		console.log("click");
+		clearSearch();
+	});
 }
 
-/////
-// function calls
-/////
-var firstTime = true
+function clearSearch(){
+	$(".prevSearch").remove();
+	list = [];
+}
 
-// $("#search-button").on("click", _.debounce(()=>{searchRecipe(searchTerm)}, 10000));
+//array for autocomplete... feel free to add additional search terms
+$(function () {
+	var dessertSuggestions = [
+		"cupcake",
+		"cake",
+		"candy",
+		"pastry",
+		"sweet",
+		"cookie",
+		"ice cream",
+		"pie",
+		"pudding",
+		"tart",
+		"croissant",
+		"sweet roll",
+		"muffin",
+		"lollipop",
+		"caramels",
+		"lemon drops",
+		"toffee",
+		"chocolate",
+		"jujubes",
+		"jelly",
+		"jelly beans",
+		"marshmallow",
+		"marzipan",
+		"halvah",
+		"gummies",
+		"gummi bears",
+		"tootsie roll",
+		"liquorice",
+		"chupa chups",
+		"bonbon",
+		"dessert",
+		"sugar plum",
+		"drag\xe9e",
+		"apple pie",
+		"biscuit",
+		"wafer",
+		"chocolate bar",
+		"candy canes",
+		"gingerbread",
+		"donut",
+		"cheesecake",
+		"oat cake",
+		"carrot cake",
+		"fruitcake",
+		"souffl\xe9",
+		"tiramisu",
+		"chocolate cake",
+		"brownie",
+		"macaroon",
+		"icing",
+		"powder",
+		"topping",
+		"jelly-o",
+		"cake",
+		"sesame snaps",
+		"danish",
+		"bear claw",
+		"cotton candy",
+		"caramel corn",
+		"apple",
+		"pear",
+		"kiwi",
+		"banana",
+		"blackberry",
+		"strawberry",
+		"blueberry",
+		"lingonberry",
+		"gooseberry",
+		"boysenberry",
+		"cranberry",
+		"goji",
+		"acai",
+		"currant",
+		"mulberry",
+		"watermelon",
+		"coconut",
+		"lime",
+		"mango",
+		"orange",
+		"basil",
+		"tropical fruit",
+		"key lime",
+		"almond",
+		"rhubarb",
+		"pecan",
+		"plum",
+		"peach",
+		"spiced",
+		"granola",
+		"basalmic",
+		"nutella",
+		"vanilla bean"
+	];
+	$("#search-term").autocomplete({
+		source: dessertSuggestions
+	});
+});
+
 /////
 //events
 /////
 
-$("#search-button").on("click", function(event) {
+$("#search-button").on("click", function (event) {
 	event.preventDefault();
-
+	
 	// moved searchTerm from global to inside of event 12/5 jdr
-	var searchTerm = $("#search-term")
-		.val()
-		.trim();
-
+	var searchTerm = $("#search-term").val().trim();
+	
+	var containsTerm = false;
+	
+	if (list != null) {
+		$(list).each(function (x) {
+			if (list[x] === searchTerm) {
+				containsTerm = true;
+			}
+		});
+	}
+	
 	//push search term to the list array
-	list.push(searchTerm);
-
-	// //appending buttons to new unordered list
-	// for (var i = 0; i < list.length; i++) {
-	// 	$("#stored").append($("<br>"));
-	// 	//creating a button and setting the user input value to button
-	// 	//replaces all dashes with spaces
-	// 	userSearched = $("<button>").text(list[i]);
-	// 	console.log(userSearched);
-
-	// 	//adding class to new buttons
-	// 	// userSearched.addClass("ul");
-	// 	//string replace function, to replace all spaces with dashes
-	// 	userSearched.attr("id", list[i]);
-	// 	$("#stored").append(userSearched);
-	// }
-
-	//need to use what Jody grabs from the recipe to store to local storage
-	//put three recipies in an array when they are grabbed, and then store that array with the search term
+	if (containsTerm === false) {
+		list.push(searchTerm);
+	}
+	
+	searchRecipe(searchTerm);
+	// console.log(searchRecipe(searchTerm));
+	
 	localStorage.setItem("key word", JSON.stringify(searchTerm));
-	//need to loop through what API has returned and create array of returns to store in local storage
 	//clearing the search bar
+	
 	$("#search-term").val("");
 
-	sideButtons();
-	//passed search term to function 12/5 826am - jdr
-	// if (firstTime === true){ 
-	// 	searchRecipe(searchTerm);
-	// 	firstTime = false
-	// } else {
-	// 	_.debounce(()=>{searchRecipe(searchTerm)}, 3000);
-	// }
-	searchRecipe(searchTerm);
+
 });
 
+	//passed search term to function 12/5 826am - jdr
